43a2d7ad5b35cce4d32c234bb33438c1cd3d89bb
Eli <elijah.fernandez@tlc.texas.gov>
2023-01-20 11:23:33 -0600

    @angular/material migration - migration-v15
    Updates the Angular Material to v15

--- projects/ix-libs/src/lib/ix-scroll/ix-scroll.module.ts
+++ projects/ix-libs/src/lib/ix-scroll/ix-scroll.module.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ScrollTopButtonComponent } from './ix-scroll.component';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
