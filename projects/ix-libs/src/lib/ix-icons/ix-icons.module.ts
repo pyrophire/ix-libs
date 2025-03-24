d0ff8205167322e54fb467b6153ba78914df0f50
Eli <elijah.fernandez@tlc.texas.gov>
2025-03-24 16:48:23 -0500

    Bump version to 19.3.1 and add new SVG icons for CSV and download functionality

--- projects/ix-libs/src/lib/ix-icons/ix-icons.module.ts
+++ projects/ix-libs/src/lib/ix-icons/ix-icons.module.ts
@@ -4,34 +4,18 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
-  exports: [],
+  exports: []
 })
 export class IxIconsModule {
   // To use: <mat-icon svgIcon="ix-file-pdf"></mat-icon>
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'ix-file-pdf',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_pdf.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'ix-file-doc',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_doc.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'ix-file-excel',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_excel.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'ix-file-html',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_html.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'ix-export',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/export.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'ix-dialog',
-      sanitizer.bypassSecurityTrustResourceUrl('ix-img/dialog.svg')
-    );
+    iconRegistry.addSvgIcon('ix-file-pdf', sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_pdf.svg'));
+    iconRegistry.addSvgIcon('ix-file-doc', sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_doc.svg'));
+    iconRegistry.addSvgIcon('ix-file-excel', sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_excel.svg'));
+    iconRegistry.addSvgIcon('ix-file-html', sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_html.svg'));
+    iconRegistry.addSvgIcon('ix-file-csv', sanitizer.bypassSecurityTrustResourceUrl('ix-img/file_csv.svg'));
+    iconRegistry.addSvgIcon('ix-save-csv', sanitizer.bypassSecurityTrustResourceUrl('ix-img/download_csv.svg'));
+    iconRegistry.addSvgIcon('ix-export', sanitizer.bypassSecurityTrustResourceUrl('ix-img/export.svg'));
+    iconRegistry.addSvgIcon('ix-dialog', sanitizer.bypassSecurityTrustResourceUrl('ix-img/dialog.svg'));
   }
 }
